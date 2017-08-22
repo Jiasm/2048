d1b9606e1572b0dadeccdacfac4ef7e0078bb9cb
jiashunming <jiashunming@outlook.com>
2017-08-22 22:05:18 +0800

    update:添加触摸事件的支持

--- src/index.js
+++ src/index.js
@@ -25,3 +25,41 @@ window.addEventListener('keyup', ({keyCode}) => {
       break
   }
 })
+
+let startPoint = null
+let endPoint = null
+
+window.addEventListener('touchstart', (e) => {
+  startPoint = {
+    x: e.touches[0].screenX,
+    y: e.touches[0].screenY
+  }
+  endPoint = null
+})
+
+window.addEventListener('touchend', (e) => {
+  endPoint = {
+    x: e.changedTouches[0].screenX,
+    y: e.changedTouches[0].screenY
+  }
+
+  if (startPoint && endPoint) {
+    switch (true) {
+      case startPoint.y > endPoint.y: // 上
+        gameController.move({direction: 'top'})
+        break
+      case startPoint.x < endPoint.x: // 右
+        gameController.move({direction: 'right'})
+        break
+      case startPoint.y < endPoint.y: // 下
+        gameController.move({direction: 'bottom'})
+        break
+      case startPoint.x > endPoint.x: // 左
+        gameController.move({direction: 'left'})
+        break
+    }
+
+    startPoint = null
+    endPoint = null
+  }
+})
